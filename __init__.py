6f835e506a47d235bbe08b1f507c286bc88035df
URL <url.sequel@gmail.com>
2026-03-09 23:19:12 +0200

    ADDED init so python can find the module

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""__init__ file for the main module"""
+__author__ = "Urho Laurinen"
+__copyright__ = "(c) Urho Laurinen 2026"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
+from .proxmox_helper import ProxmoxHelper
